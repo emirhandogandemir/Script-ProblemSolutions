0c9b4ef19bf5df8319cf2421d41bc6bb205ddeb7
emirhandogandemir <emirhandgndmr51@gmail.com>
2021-11-06 02:45:52 -0700

    readingInputexamples has been added to repository

---
+++ readingInputexamples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#reading input
+# Emirhan Dogandemir 6/11/2021
+
+#example 1
+echo "Please enter your name:"
+read name
+echo "Welcome to SWE Dept ,Ktu  $name "
+
+#example 2
+
+echo "please enter your name and surname"
+read name1 name2
+echo "welcome to swe dept, ktu, $name1 $name2"
