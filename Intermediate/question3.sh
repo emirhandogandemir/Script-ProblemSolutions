0968874b4b9f70008387058bd1bc88f80d3d1e7d
emirhandogandemir <emirhandgndmr51@gmail.com>
2021-11-07 01:46:52 -0700

    solution of question3.sh has been added to Intermediate file

--- Intermediate/question3.sh
+++ Intermediate/question3.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 #Emirhan Dogandemir 7/11/2021
 #calisma dizinindeki public(word) izni sadece okuma olan dizinlerin sayisini verecek bash kodunu yaziniz
+cd /home/emirhan
+ls -l > onlyreadpublic.txt && egrep ".(r|-)(w|-)(x|-)(r|-)(w|-)(x|-)r--" onlyreadpublic.txt
 
+# ilk basta dizinimi sectim 
+# ls -l komutunun ciktisini onlyreadpublic.txt icine koyup bu command satirinin basarili olmasi durumunda gerekli Regular expressionu yazarak istenen sonucu cikti olarak verdim 
