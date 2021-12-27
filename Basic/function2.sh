820ce1c8704df9db7620b8b7c4a75e8826b982b0
emirhandogandemir <emirhandgndmr51@gmail.com>
2021-12-27 05:48:19 -0800

    function2 and function3 added

---
+++ Basic/function2.sh
@@ -0,0 +1,9 @@
+function demo()
+{
+echo "All arguments to function demo():$*"
+echo "First argument $1"
+echo "Second argument $2"
+echo "Third argument $3"
+return
+}
+
---
+++ Basic/function3.sh
@@ -0,0 +1,11 @@
+function add_two()
+ {
+(( sum = $1+$2 ))
+#return $sum
+echo $sum
+ 
+}
+
+#sonuc=$(add_two $1 $2)
+#echo $sonuc
+#echo $?
