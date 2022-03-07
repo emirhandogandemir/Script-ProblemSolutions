90c7f90428d2d5e14d024e5cf7ea13cf42b48427
emirhandogandemir <emirhandgndmr51@gmail.com>
2022-03-07 09:47:55 +0300

    script has been modified

--- Advance/nginxCase.sh
+++ Advance/nginxCase.sh
@@ -11,11 +11,13 @@ FILE=/tmp/alerts
 if [ -f "$FILE" ]; then
 echo "$FILE exist"
 cd /tmp
-"echo `date` nginx kapalıdır">>alerts
+date >> alerts
+echo "nginx kapalıdır" >> alerts
 else
 cd /tmp
 touch alerts
-"echo `date` nginx kapalıdır">>alerts
+date >> alerts
+echo "nginx kapalıdır" >> alerts
 fi 
 fi
 EOF
